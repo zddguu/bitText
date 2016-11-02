d0c040710a91178992b2cb3a7cecc908d854cd93
Mark Carman <mittens.au@gmail.com>
2016-11-02 14:45:47 +1030

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+            Console.WriteLine("Debxug test");
+            String hello = "aaa";
+            
+        }
+    }
+}
